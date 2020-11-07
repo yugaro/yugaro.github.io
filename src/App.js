f547fc331e00a0e5d4a610c826cc612bcb4d7d1a
yugaro <yugaro.yugaro@gmail.com>
2020-11-07 20:21:15 +0900

    Initital commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload111.
+          Edit <code>src/App.js</code> and save to reload1112.
         </p>
         <a
           className="App-link"
