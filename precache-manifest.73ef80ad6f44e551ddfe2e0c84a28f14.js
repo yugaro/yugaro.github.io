0272bfa8a5e5b2c755b5be31f511db32a55d022f
yugaro <yugaro.yugaro@gmail.com>
2020-11-07 20:06:14 +0900

    Updates

---
+++ precache-manifest.73ef80ad6f44e551ddfe2e0c84a28f14.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba318ff907e50ff64e640e8b8a2c242",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15f7b2d383ed41e4a6cc",
+    "url": "/static/css/main.15e30b0f.chunk.css"
+  },
+  {
+    "revision": "3a0d3d9e699d727f8059",
+    "url": "/static/js/2.78ee3d02.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.78ee3d02.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15f7b2d383ed41e4a6cc",
+    "url": "/static/js/main.62e92eaa.chunk.js"
+  },
+  {
+    "revision": "99a1df5724a42cc4fef3",
+    "url": "/static/js/runtime-main.4a3acf61.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
