ee8952843a9513a63e2fe045d649dc295d62cf63
yugaro <yugaro.yugaro@gmail.com>
2020-11-08 19:48:13 +0900

    Initial commit

---
+++ build/precache-manifest.8c78243ab0299ad8c8de24b48c96922a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca4afdc08400469f8ea5e1f50e856ab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b44576ed3c67d94cfcf6",
+    "url": "/static/css/main.e24ea4c6.chunk.css"
+  },
+  {
+    "revision": "dc97552d124bfd77555a",
+    "url": "/static/js/2.699d8418.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.699d8418.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b44576ed3c67d94cfcf6",
+    "url": "/static/js/main.67292719.chunk.js"
+  },
+  {
+    "revision": "99a1df5724a42cc4fef3",
+    "url": "/static/js/runtime-main.4a3acf61.js"
+  },
+  {
+    "revision": "89c2c0674bdba3d61daf6b1b1e5e82b5",
+    "url": "/static/media/github-logo2.89c2c067.png"
+  },
+  {
+    "revision": "efbac42603179e82f1e87f6dd0bad715",
+    "url": "/static/media/instagram.efbac426.png"
+  },
+  {
+    "revision": "e0a3ddeed1fd4e65c2810cf888ec2403",
+    "url": "/static/media/linkedin.e0a3ddee.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "619adca33baa255d37514ebd102da6d2",
+    "url": "/static/media/myicon.619adca3.png"
+  },
+  {
+    "revision": "af6c63c5080ee5b5a3b496902afc5aa3",
+    "url": "/static/media/top.af6c63c5.jpg"
+  },
+  {
+    "revision": "d7c6644ae020495730a8f1fee7924990",
+    "url": "/static/media/twitter.d7c6644a.png"
+  }
+]);
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import './Header.css';
 import myicon from './image/myicon.png';
-import top_img from './image/top.png';
-import github_icon from './image/github-logo_icon.png';
+import top_img from './image/top.jpg';
+import github_icon from './image/github-logo2.png';
 import logo from './image/logo.svg';
+import instagram_icon from './image/instagram.png'
+import twitter_icon from './image/twitter.png'
+import linkedin_icon from './image/linkedin.png'
+import mail_icon from './image/mail.png'
 
 class Header extends React.Component {
   render() {
     return (
      <div className="header">
       <header className="App-header">
+        <div className="App-header-top-containter">
+          <div className="App-header-top-containter-right">
+            <img src={mail_icon} className="App-logo-header mail_icon_img" alt="logo" />
+            <p className="mail_fukidashi">Click to copy my email address to your clipboard</p>
+            <p className="mail_fukidashi2">Copied!!</p>
+           <a href = "https://github.com/yugaro" className = "btn btn-head2"> <img src={github_icon} className="App-logo-header" alt="logo" /> </a>
+           <a href = "https://www.instagram.com/garoyugaro/" className = "btn btn-head2" > <img src={instagram_icon} className="App-logo-header" alt="logo"/></a>
+           <a href = "https://twitter.com/garoyugaro/" className = "btn btn-head2" > <img src={twitter_icon} className="App-logo-header" alt="logo" /> </a>
+           <a href = "https://www.linkedin.com/in/yuga-onoue-8934531a8/" className = "btn btn-head2" > <img src={linkedin_icon} className="App-logo-header" alt="logo" /></a>
+          </div>
+        </div>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello!
+        </p>
+        <p>
+          I'm Yuga Onoue!
         </p>
         <a
           className="App-link"
