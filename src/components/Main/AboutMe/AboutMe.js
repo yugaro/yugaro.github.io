a8882a35c1722e9f71dc66f7b246c2d2437a1c6e
yugaro <yugaro.yugaro@gmail.com>
2021-02-06 10:40:18 +0900

    Initial Commit

--- src/components/Main/AboutMe/AboutMe.js
+++ src/components/Main/AboutMe/AboutMe.js
@@ -16,7 +16,7 @@ class AboutMe extends React.Component {
 	          About Me
 	          </div>
 	          <div className='tctfl about-me-contents'>
-	           I'm a bachelor at Osaka University majoring in intelligent systems, which are synthesized by mathematics, hardware, and software engineering, etc. The areas of my particular concerns are cyber-physical systems (CPS), where physical and server systems densely communicate with each other, and I develop their analysis and synthesis toward safe, stable, and low cost, based on machine learning, control theory, and computer science.
+	           I'm a bachelor at Osaka University majoring in intelligent systems, which are synthesized by mathematics, hardware, and software engineering, etc. The areas of my particular concerns are cyber-physical systems (CPS), where physical and software components are densely intertwined with each other, and I develop their analysis and synthesis toward safe, stable, and low cost, based on machine learning, control theory, and computer science.
 	          The unique knowledge and interdisciplinary experienced skill sets as an engineer allow me to merge multidisciplinary resources, from the theoretical developments to the applications.
 	           I hope to develop further my skills, manage a team, and provide directions that align with the overall goals of the company.
 	           </div>
